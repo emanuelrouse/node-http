76e66504471e3ee330c64e3158cc60473c5bfc03
Emanuel Rouse <41143874+emanuelrouse@users.noreply.github.com>
2022-01-23 11:43:41 -0500

    Node HTTP Example 2

--- server.js
+++ server.js
@@ -7,10 +7,39 @@ const path = require('path'); // import path(core) module
 const fs = require('fs');  // import fs(core) module
 
 const server = http.createServer((req, res) => {
-    console.log(req.headers);
-    res.statusCode = 200; // send a ok response back to the server
-    res.setHeader('Content-Type', 'text/html'); // client: What kind of data to expect in the response body? 
-    res.end('<html><body><h1>Hello World!</h1></body></html>'); // close the response stream
+    console.log(`Request for ${req.url} by method ${req.method}`); // log out the url and the method used for the reqest
+
+    if (req.method === 'GET') {
+        let fileUrl = req.url; // if the request comes in just to the hostname and not hostname/something.html the req url will just be a forward slash
+        if (fileUrl === '/') {
+            fileUrl = '/index.html';
+        }
+
+        // path.resolve converts path from a relative path to a absolute path
+        const filePath = path.resolve('./public' + fileUrl);
+        const fileExt = path.extname(filePath); // parse 
+        if (fileExt === '.html') {
+            fs.access(filePath, err => {
+                if (err) {
+                    res.statusCode = 404;
+                    res.setHeader('Content-Type', 'text/html');
+                    res.end(`<html><body><h1>Error 404: ${fileUrl} is not found</h1></body></html>`);
+                    return; // stop code after this from executing
+                }
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'text/html');
+                fs.createReadStream(filePath).pipe(res); // don't load the whole file into memeory, load in small chunks. two streams allow you to pipe one to another
+            });
+        } else {
+            res.statusCode = 404;
+            res.setHeader('Content-Type', 'text/html');
+            res.end(`<html><body><h1>Error 404: ${fileUrl} is not an HTML file</h1></body></html>`);
+        }
+    } else {
+        res.statusCode = 404;
+        res.setHeader('Content-Type', 'text/html');
+        res.end(`<html><body><h1>Error 404: ${req.method} not supported</h1></body></html>`);
+    }
 });
 // create a new instance of the Http.Server class
 server.listen(port, hostname, () => {
