0706252ef9dfd817227080c7f70b0c74b5552097
Emanuel Rouse <41143874+emanuelrouse@users.noreply.github.com>
2022-01-23 10:34:24 -0500

    Added Comments to Node HTTP Example 1

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
-const http = require('http');
+const http = require('http'); // require http core module
 
-const hostname = 'localhost';
-const port = 3000;
+const hostname = 'localhost'; // set hostname to localhost
+const port = 3000; // set the port number to 3000
 
 const server = http.createServer((req, res) => {
     console.log(req.headers);
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.end('<html><body><h1>Hello World!</h1></body></html>');
+    res.statusCode = 200; // send a ok response back to the server
+    res.setHeader('Content-Type', 'text/html'); // client: What kind of data to expect in the response body? 
+    res.end('<html><body><h1>Hello World!</h1></body></html>'); // close the response stream
 });
-
+// create a new instance of the Http.Server class
 server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
-});
